d19b38b707ba42edc64b20e57fd5b6f8481350f2
k.pavithra <k.pavithrakasinadhan@mail.com>
2024-06-04 11:44:40 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CRUDConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
